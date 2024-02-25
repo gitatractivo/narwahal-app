58a964912c011b9b191ff53f12fa5e327308058a
gitatractivo <gitanshutalwar@gmail.com>
2024-02-26 03:04:57 +0530

    checkInComponent

--- src/components/common/CheckInSpareListItem.tsx
+++ src/components/common/CheckInSpareListItem.tsx
@@ -1,54 +1,61 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import {DetailListItemProps} from '../../interface/common';
-import {colors, fontSize, fonts, hp, wp} from '../../helper';
+import { DetailListItemProps } from '../../interface/common';
+import { colors, fontSize, fonts, hp, wp } from '../../helper';
 
-const CheckInSpareList = ({item, onPress}: any) => {
+const CheckInSpareList = ({ item, onPress }: any) => {
   return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={onPress}
+    <View
       style={styles.container}>
       <Text style={styles.titleText}>{item?.title}</Text>
       <Text style={styles.descText}>{item?.desc}</Text>
-      <View style={styles.tagContainer}>
-        <View
-          style={[
-            styles.tagView,
-            {
-              backgroundColor:
-                item?.tag == 'Reconditioned'
-                  ? colors.orangexLight
-                  : colors.greenxLight,
-              borderColor:
-                item?.tag == 'Reconditioned'
-                  ? colors.orangeLight
-                  : colors.greenLight,
-            },
-          ]}>
-          <Text
+      
+
+      <TouchableOpacity activeOpacity={0.7} onPress={onPress} style={styles.innerDiv}>
+        <View style={styles.innerHeading}>
+          <Text style={styles.innerHeadingText}>ID: View Details</Text>
+        </View>
+        <View style={styles.innerContainer}>
+          <View style={styles.flexContainer}>
+
+            <View style={styles.spanContainer}>
+              <Text >Scanned Qty:</Text>
+              <Text style={styles.valueText}>10</Text>
+            </View>
+            <View style={styles.spanContainer}>
+              <Text >Check-in Qty:</Text>
+              <Text style={styles.valueText}>5</Text>
+            </View>
+          </View>
+          <View
             style={[
-              styles.tagText,
+              styles.tagView,
               {
-                color:
-                  item?.tag == 'Reconditioned' ? colors.orange : colors.green,
+                backgroundColor:
+                  item?.tag == 'Reconditioned'
+                    ? colors.orangexLight
+                    : colors.greenxLight,
+                borderColor:
+                  item?.tag == 'Reconditioned'
+                    ? colors.orangeLight
+                    : colors.greenLight,
               },
             ]}>
-            {item?.tag}
-          </Text>
+            <Text
+              style={[
+                styles.tagText,
+                {
+                  color:
+                    item?.tag == 'Reconditioned' ? colors.orange : colors.green,
+                },
+              ]}>
+              {item?.tag}
+            </Text>
+          </View>
         </View>
-        <Text style={styles.uniqText}>{item?.uniqueID}</Text>
-      </View>
-
-      <View style={styles.qtyContainer}>
-        <Text style={styles.uniqText}>{`Scanned Qty:`}</Text>
-        <Text style={styles.qtyText}>{item?.scannedQty}</Text>
-        <Text
-          style={[styles.uniqText, styles.marginLeft]}>{`Checkin Qty:`}</Text>
-        <Text style={styles.qtyText}>{item?.checkinQty}</Text>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -59,6 +66,9 @@ const styles = StyleSheet.create({
     padding: wp(5.5),
     borderBottomWidth: wp(0.2),
     borderBottomColor: colors.grey,
+    flex:1,
+    flexDirection: 'column',
+    gap: wp(1.5),
   },
   titleText: {
     color: colors.black,
@@ -78,7 +88,6 @@ const styles = StyleSheet.create({
   },
   tagView: {
     borderWidth: wp(0.2),
-    marginRight: wp(4.5),
     paddingVertical: wp(1),
     borderRadius: wp(1.33),
     paddingHorizontal: wp(3),
@@ -116,4 +125,53 @@ const styles = StyleSheet.create({
   marginLeft: {
     marginLeft: wp(3),
   },
+  innerDiv: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: wp(2),
+    borderWidth: 0.5,
+    borderColor: '#dddddd',
+    overflow: 'hidden',
+  },
+  innerHeading: {
+    width: '100%',
+    paddingVertical: wp(1.5),
+    paddingHorizontal: wp(5),
+    backgroundColor: "#e5f2fd",
+
+  },
+  innerHeadingText: {
+    color: "#47AFFF",
+    fontSize: fontSize(15),
+    fontFamily: fonts.bold,
+  },
+  innerContainer: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: wp(5),
+    paddingVertical: wp(3),
+    backgroundColor: colors.white,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  spanContainer: {
+    flex: 1,
+    gap: wp(.5),
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: 'auto'
+  }, valueText: {
+    color: colors.black,
+    // opacity: 0.9
+  },
+  flexContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: 231,
+    // minWidth: 231,
+
+  }
 });
